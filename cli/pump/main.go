3602ce7c7e709c44b374db812bfcedd4021d3718
Mark Eschbach <meschbach@gmail.com>
2022-01-02 21:42:38 -0800

    extracts reusable pump to process stdout

--- cli/pump/main.go
+++ cli/pump/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/meschbach/go-junk-bucket/sub"
 	"os"
-	"sync"
 )
 
 func main() {
@@ -16,26 +15,7 @@ func main() {
 		return
 	}
 	cmd := sub.NewSubcommand(args[1], args[2:])
-
-	var pumpsDone sync.WaitGroup
-
-	toClose := make([]chan string, 0)
-	buildPump := func(name string) chan string {
-		pipe := make(chan string, 8)
-		toClose = append(toClose, pipe)
-		pumpsDone.Add(1)
-		go sub.PumpPrefixedChannel(name, pipe, &pumpsDone)
-		return pipe
+	if err := cmd.PumpToStandard(args[1]); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed: %s\n", err.Error())
 	}
-
-	stdout := buildPump("<<stdout>>")
-	stderr := buildPump("<<stderr>>")
-
-	if err := cmd.Run(stdout, stderr); err != nil {
-		panic(err)
-	}
-	for _, pipe := range toClose {
-		close(pipe)
-	}
-	pumpsDone.Wait()
 }
---
+++ sub/convenience.go
@@ -0,0 +1,38 @@
+package sub
+
+import (
+	"fmt"
+	"sync"
+)
+
+//PumpToStandard will run the given command
+func (s *Subcommand) PumpToStandard(title string) error {
+	var pumpsDone sync.WaitGroup
+
+	toClose := make([]chan string, 0)
+	buildPump := func(name string) chan string {
+		pipe := make(chan string, 8)
+		toClose = append(toClose, pipe)
+		pumpsDone.Add(1)
+		go PumpPrefixedChannel(name, pipe, &pumpsDone)
+		return pipe
+	}
+
+	defer func() {
+		for _, pipe := range toClose {
+			close(pipe)
+		}
+	}()
+
+	stdoutName := fmt.Sprintf("<<%s.stdout>>", title)
+	stdout := buildPump(stdoutName)
+
+	stderrName := fmt.Sprintf("<<%s.stderr>>", title)
+	stderr := buildPump(stderrName)
+
+	if err := s.Run(stdout, stderr); err != nil {
+		return err
+	}
+	pumpsDone.Wait()
+	return nil
+}
