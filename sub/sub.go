c3aadc0aea3d6e5b2a70b04be8d1b79ef0356560
Mark Eschbach <meschbach@gmail.com>
2022-01-08 20:37:38 -0800

    sub/io: allow interaction with target processes

--- sub/sub.go
+++ sub/sub.go
@@ -1,6 +1,7 @@
 package sub
 
 import (
+	"fmt"
 	"os/exec"
 	"sync"
 )
@@ -26,25 +27,39 @@ func (s *Subcommand) WithOption(opt Option)  {
 	s.Options = append(s.Options, opt)
 }
 
-func (s *Subcommand) Run(stdout chan<- string, stderr chan<- string) error {
+func (s *Subcommand) Interact(stdin <-chan string, stdout chan<- string, stderr chan<- string ) error {
+	var completedReading sync.WaitGroup
+	var readyGate sync.WaitGroup
+
 	cmd := exec.Command(s.programName, s.programArguments...)
 	for _, option := range s.Options {
 		option.Customize(cmd)
 	}
 
-	stdin, err := cmd.StdinPipe()
+	stdinPipe, err := cmd.StdinPipe()
 	if err != nil {
 		return err
 	}
-	if err := stdin.Close(); err != nil {
-		return err
-	}
+	readyGate.Add(1)
+	completedReading.Add(1)
+	go func() {
+		readyGate.Done()
+		for in := range stdin {
+			fmt.Printf("<<stdin>> %q\n", in)
+			withNewLine := in + "\n"
+			asBytes := []byte(withNewLine)
+			_, err := stdinPipe.Write(asBytes)
+			if err != nil { panic(err) }
+		}
+		err := stdinPipe.Close()
+		if err != nil { panic(err) }
+		completedReading.Done()
+	}()
 
-	stdoutPipe, _ := cmd.StdoutPipe()
-	stderrPipe, _ := cmd.StderrPipe()
-
-	var completedReading sync.WaitGroup
-	var readyGate sync.WaitGroup
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil { return err }
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil { return err }
 
 	completedReading.Add(1)
 	readyGate.Add(1)
@@ -61,4 +76,11 @@ func (s *Subcommand) Run(stdout chan<- string, stderr chan<- string) error {
 
 	completedReading.Wait()
 	return err
+}
+
+func (s *Subcommand) Run(stdout chan<- string, stderr chan<- string) error {
+	stdin := make(chan string)
+	close(stdin)
+
+	return s.Interact(stdin,stdout,stderr)
 }
