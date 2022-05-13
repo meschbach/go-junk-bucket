7b0c8f64b7a60998b59f555204d2f154be11b474
Mark Eschbach <meschbach@gmail.com>
2022-05-12 19:40:45 -0700

    fx/filter: moving from another project

---
+++ pkg/fx/fx.go
@@ -0,0 +1,12 @@
+package fx
+
+//Filter returns a subset of elements for each test which is true
+func Filter[E any](elements []E, test func(e E) bool) []E {
+	out := make([]E, 0)
+	for _, e := range elements {
+		if test(e) {
+			out = append(out, e)
+		}
+	}
+	return out
+}
