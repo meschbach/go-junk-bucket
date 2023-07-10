5460b6910f17f9b064d7ff5e2899341f6572ee38
Mark Eschbach <meschbach@gmail.com>
2023-07-10 10:08:48 -0700

    fx/split: first implementation

--- pkg/fx/fx.go
+++ pkg/fx/fx.go
@@ -11,6 +11,22 @@ func Filter[E any](elements []E, test func(e E) bool) []E {
 	return out
 }
 
+// Split applies all elements e to test.  All e which test returns true are in the first slice, all others are
+// in the second slice. Useful for splitting a slice into two buckets to be further operated on.  Filter discards the
+// negative results of test which is more efficient if no further processing needs to occur.
+func Split[E any](elements []E, test func(e E) bool) ([]E, []E) {
+	left := make([]E, 0)
+	right := make([]E, 0)
+	for _, e := range elements {
+		if test(e) {
+			left = append(left, e)
+		} else {
+			right = append(right, e)
+		}
+	}
+	return left, right
+}
+
 // Map transforms a slice of inputs through the transform function to result in an equal sized slice of outputs.
 func Map[I any, O any](inputs []I, transform func(i I) O) []O {
 	inputLength := len(inputs)
