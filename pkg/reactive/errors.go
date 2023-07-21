44367e4eb144c0aca0b0d5b165e1a92f3dd42f5c
Mark Eschbach <meschbach@gmail.com>
2023-07-21 10:15:20 -0700

    reactive/sink: documents sink interface

--- pkg/reactive/errors.go
+++ pkg/reactive/errors.go
@@ -2,5 +2,10 @@ package reactive
 
 import "errors"
 
+// Done represents a writable stream which has been closed.  A Sink may return this in the time between being instructed
+// to close and still draining all elements from their buffer
 var Done = errors.New("done")
+
+// Full represents a writable stream whose internal buffers which have been filled.  A Full error provides a clear
+// signal for backpressure on the emitting source.
 var Full = errors.New("full")
--- pkg/reactive/sink.go
+++ pkg/reactive/sink.go
@@ -12,8 +12,10 @@ type Sink[T any] interface {
 	// * Done - If the stream has finished.  No further writes will be consumed
 	// * Full - Buffers are currently full and unable to accept another value.  Listen for OnDrain for additional capacity.
 	Write(ctx context.Context, v T) error
+	// Finish instructs the sink to no longer accept writes and flush internal buffers and clean up.
 	Finish(ctx context.Context) error
 	SinkEvents() *SinkEvents[T]
 
+	// Resume instructs the Sink to resume draining internal buffers and accept writes again.
 	Resume(ctx context.Context) error
 }
