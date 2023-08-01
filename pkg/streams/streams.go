1813eff1dd355cfd08d648c77fbc2a7cc013eaeb
Mark Eschbach <meschbach@gmail.com>
2023-07-31 20:25:44 -0700

    doc/streams: inital cut of the streams package

---
+++ pkg/streams/streams.go
@@ -0,0 +1,4 @@
+// Package streams provides a set of Source and Sink semantics to plug together object streams.  Streams are closer to
+// NodeJS object stream semantics as opposed to bufio byte level semantics.  Although nothing stops one from putting
+// together a byte stream.
+package streams
