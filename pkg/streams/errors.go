04ce5171f8df93af19958f9259bddab7df3c38e6
Mark Eschbach <meschbach@gmail.com>
2023-11-28 16:10:28 -0800

    streams/errors: be more explicit the errors are from streams with messages

--- pkg/streams/errors.go
+++ pkg/streams/errors.go
@@ -4,8 +4,8 @@ import "errors"
 
 // Done represents a writable stream which has been closed.  A Sink may return this in the time between being instructed
 // to close and still draining all elements from their buffer
-var Done = errors.New("done")
+var Done = errors.New("stream done")
 
 // Full represents a writable stream whose internal buffers which have been filled.  A Full error provides a clear
 // signal for backpressure on the emitting source.
-var Full = errors.New("full")
+var Full = errors.New("stream full")
