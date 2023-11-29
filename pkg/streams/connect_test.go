58677933d25d6da4b9fd1db838dd473a7cfe89e4
Mark Eschbach <meschbach@gmail.com>
2023-11-28 16:03:33 -0800

    streams/connect: test to connect two streams using buffers

---
+++ pkg/streams/connect_test.go
@@ -0,0 +1,43 @@
+package streams
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestConnect(t *testing.T) {
+
+	//todo: really testing Connect
+	t.Run("Given two connected streams with elements passed", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, done := context.WithTimeout(context.Background(), 1*time.Second)
+		t.Cleanup(done)
+
+		source := NewBuffer[int](8)
+		sink := NewBuffer[int](8)
+
+		_, err := Connect[int](ctx, source, sink)
+		require.NoError(t, err)
+		exampleValues := []int{0, 1, 2, 3, 4, 5, 6, 7}
+		for _, v := range exampleValues {
+			require.NoError(t, source.Write(ctx, v))
+		}
+
+		t.Run("When finishing the source", func(t *testing.T) {
+			require.NoError(t, source.Finish(ctx))
+
+			t.Run("Then the sink receives all elements", func(t *testing.T) {
+				assert.Equal(t, exampleValues, sink.Output)
+			})
+
+			t.Run("Then the sink is also finished", func(t *testing.T) {
+				assert.Equal(t, bufferFinished, sink.state, fmt.Sprintf("expected state is finished, got %s", sink.state.String()))
+			})
+		})
+	})
+}
