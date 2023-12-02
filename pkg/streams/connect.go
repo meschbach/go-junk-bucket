e77dfe1e9f3affc1956a72cf829b6865b8fae82d
Mark Eschbach <meschbach@gmail.com>
2023-12-01 20:03:09 -0800

    streams/connect: allows naming of a Connect call for tracing

--- pkg/streams/connect.go
+++ pkg/streams/connect.go
@@ -2,10 +2,42 @@ package streams
 
 import (
 	"context"
+	"errors"
 	"github.com/meschbach/go-junk-bucket/pkg/emitter"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 )
+
+type ConnectedPipeOption func(c *connectedPipeConfig) error
+
+func WithTraceAttributes(attrs ...attribute.KeyValue) ConnectedPipeOption {
+	return func(c *connectedPipeConfig) error {
+		c.traceAttributes = attrs
+		return nil
+	}
+}
+func WithTracePrefix(tracePrefix string) ConnectedPipeOption {
+	return func(c *connectedPipeConfig) error {
+		c.traceEventPrefix = tracePrefix
+		return nil
+	}
+}
+
+type connectedPipeConfig struct {
+	traceAttributes  []attribute.KeyValue
+	traceEventPrefix string
+}
+
+func (c *connectedPipeConfig) init(opts []ConnectedPipeOption) error {
+	var errs []error
+	for _, opt := range opts {
+		errs = append(errs, opt(c))
+	}
+	if errs != nil {
+		return errors.Join(errs...)
+	}
+	return nil
+}
 
 type ConnectedPipe[E any] struct {
 	from Source[E]
@@ -14,11 +46,12 @@ type ConnectedPipe[E any] struct {
 	onSourceData     *emitter.Subscription[E]
 	onSourceFinished *emitter.Subscription[Source[E]]
 	drain            *emitter.Subscription[Sink[E]]
+	config           connectedPipeConfig
 }
 
 func (c *ConnectedPipe[E]) Close(ctx context.Context) error {
 	span := trace.SpanFromContext(ctx)
-	span.AddEvent("ConnectedPipe.Close")
+	span.AddEvent(c.config.traceEventPrefix+".Close", trace.WithAttributes(c.config.traceAttributes...))
 
 	if err := c.from.Pause(ctx); err != nil {
 		return err
@@ -31,32 +64,40 @@ func (c *ConnectedPipe[E]) Close(ctx context.Context) error {
 }
 
 // Connect allows events to flow from a source to a sink.
-func Connect[E any](ctx context.Context, from Source[E], to Sink[E]) (*ConnectedPipe[E], error) {
+func Connect[E any](ctx context.Context, from Source[E], to Sink[E], opts ...ConnectedPipeOption) (*ConnectedPipe[E], error) {
 	pipe := &ConnectedPipe[E]{
 		from: from,
 		sink: to,
+		config: connectedPipeConfig{
+			traceEventPrefix: "ConnectedPipe",
+		},
 	}
+	//todo: should become a default opts
+	if err := pipe.config.init(opts); err != nil {
+		return nil, err
+	}
+	opts = nil
 
-	attrs := trace.WithAttributes(attribute.Stringer("ConnectedPipe", ptrFormattedStringer[ConnectedPipe[E]]{pipe}))
+	attrs := trace.WithAttributes(pipe.config.traceAttributes...)
 	span := trace.SpanFromContext(ctx)
-	span.AddEvent("ConnectedPipe.Connect", attrs)
+	span.AddEvent(pipe.config.traceEventPrefix+".Connect", attrs)
 
 	sourceEvents := from.SourceEvents()
 	pipe.onSourceData = sourceEvents.Data.OnE(func(ctx context.Context, event E) error {
 		span := trace.SpanFromContext(ctx)
-		span.AddEvent("ConnectedPipe.Source.Data", attrs)
+		span.AddEvent(pipe.config.traceEventPrefix+".Source.Data", attrs)
 		return to.Write(ctx, event)
 	})
 	pipe.onSourceFinished = sourceEvents.End.OnE(func(ctx context.Context, event Source[E]) error {
 		span := trace.SpanFromContext(ctx)
-		span.AddEvent("ConnectedPipe.Source.End", attrs)
+		span.AddEvent(pipe.config.traceEventPrefix+".Source.End", attrs)
 		return to.Finish(ctx)
 	})
 
 	sinkEvents := to.SinkEvents()
 	pipe.drain = sinkEvents.OnDrain.OnE(func(ctx context.Context, event Sink[E]) error {
 		span := trace.SpanFromContext(ctx)
-		span.AddEvent("ConnectedPipe.Source.Data", attrs)
+		span.AddEvent(pipe.config.traceEventPrefix+".Sink.OnDrain", attrs)
 
 		return from.Resume(ctx)
 	})
