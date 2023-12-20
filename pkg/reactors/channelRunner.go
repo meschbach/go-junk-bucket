d99055629d10cf042d4b5f286ef3b5e2fa30cffa
Mark Eschbach <meschbach@gmail.com>
2023-12-20 09:40:28 -0800

    reactors/channel: do not announce channel reactor launch nor panic on CancelContext

--- pkg/reactors/channelRunner.go
+++ pkg/reactors/channelRunner.go
@@ -9,10 +9,12 @@ func RunChannelActor[E any](ctx context.Context, s E) *Channel[E] {
 	reactor, _ := NewChannel[E](32)
 	go func() {
 		_, err := reactor.ConsumeAll(ctx, s)
-		if errors.Is(err, context.Canceled) {
-			//do nothing
-		} else {
-			panic(err)
+		if err != nil {
+			if errors.Is(err, context.Canceled) {
+				//do nothing
+			} else {
+				panic(err)
+			}
 		}
 	}()
 	return reactor
